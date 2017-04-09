6616a73bc59a4865acd253581417d11c0f374bf6
shikhi7 <mshikhar29@gmail.com>
2017-04-10 03:08:17 +0530

    Added xml schema

--- XMLWriter.js
+++ XMLWriter.js
@@ -28,11 +28,6 @@ function XMLWriter()
             this.XML.push("</"+this.Nodes.pop()+">");
         this.State="";
     }
-    this.Attrib = function(Name, Value)
-    {
-        if (this.State!="beg" || !Name) return;
-        this.XML.push(" "+Name+"=\""+this.FormatXML(Value)+"\"");
-    }
     this.WriteString = function(Value)
     {
         if (this.State=="beg") this.XML.push(">");
